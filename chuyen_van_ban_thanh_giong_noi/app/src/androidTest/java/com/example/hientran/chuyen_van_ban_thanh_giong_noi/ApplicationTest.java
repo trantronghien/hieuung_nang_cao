34a1e557c1a2294092a42244af57670e3bca85d5
trantronghien <hellotru@gmail.com>
2015-12-26 10:59:33 +0700

    Chuyen_vanban
    chuyển văn bản thành giọng nói

---
+++ chuyen_van_ban_thanh_giong_noi/app/src/androidTest/java/com/example/hientran/chuyen_van_ban_thanh_giong_noi/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.hientran.chuyen_van_ban_thanh_giong_noi;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
