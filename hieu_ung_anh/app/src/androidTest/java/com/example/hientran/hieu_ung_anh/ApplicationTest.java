35d4c51cfbeb7f84edee6a054f9e9c1c8f058826
trantronghien <hellotru@gmail.com>
2015-12-26 09:52:24 +0700

    26/12
    hiệu ứng thay đổi pixel trong ảnh sử dụng lớp BitmapDrawable

---
+++ hieu_ung_anh/app/src/androidTest/java/com/example/hientran/hieu_ung_anh/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.hientran.hieu_ung_anh;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
